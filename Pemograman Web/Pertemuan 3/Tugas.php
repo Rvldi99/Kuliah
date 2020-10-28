bf4f269739b5075987cb9142cc3e207b104d8b9f
Yuniar Rivaldi Nazar <54404794+Rvldi99@users.noreply.github.com>
2020-10-28 18:12:11 +0700

    Create Tugas.php

---
+++ Pemograman Web/Pertemuan 3/Tugas.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tugas Phpin Orang Pert-3</title>
+</head>
+<body>
+<?php 
+   $Nama   ="Rivaldi Nazar Yuniar";
+   $Nim    ="17190659";
+   $Kelas  ="17.3E.07";
+   $Alamat ="Kampung Kemang - Bekasi";
+   $TTL    ="Bekasi, 7 Juli 1999";
+   $Nomor  ="088224696019";
+   $Email  ="Yuniar.Rivaldi@gmail.com";
+?>
+<table>
+   <tr>
+      <td>Nama</td>
+      <td>=</td>
+      <td><?php echo "$Nama"; ?></td>
+   </tr>
+      <tr>
+      <td>Nim</td>
+      <td>=</td>
+      <td><?php echo "$Nim"; ?></td>
+   </tr>
+      <tr>
+      <td>Kelas</td>
+      <td>=</td>
+      <td><?php echo "$Kelas"; ?></td>
+   </tr>
+      <tr>
+      <td>Alamat</td>
+      <td>=</td>
+      <td><?php echo "$Alamat"; ?></td>
+   </tr>
+      <tr>
+      <td>TTL</td>
+      <td>=</td>
+      <td><?php echo "$TTL"; ?></td>
+   </tr>
+      <tr>
+      <td>Nomor</td>
+      <td>=</td>
+      <td><?php echo "$Nomor"; ?></td>
+   </tr>
+      <tr>
+      <td>Email</td>
+      <td>=</td>
+      <td><?php echo "$Email"; ?></td>
+   </tr>
+</table>
+</body>
+</html>
