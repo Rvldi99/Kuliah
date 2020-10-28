2bad3ed9ec01a968a8985932ed454fcc7b7b2643
Yuniar Rivaldi Nazar <54404794+Rvldi99@users.noreply.github.com>
2020-10-28 18:13:22 +0700

    Create Tugas2.php

---
+++ Pemograman Web/Pertemuan 3/Tugas2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tugas Phpin Orang Pert-3</title>
+</head>
+<body>
+<?php 
+   $Panjang =5;
+   $Lebar   =10;
+   $Luas    =$Panjang*$Lebar;
+?>
+Nama Saya Rivaldi Nazar Yuniar (17190659)<br>
+Belajar Menghitung Luas Persegi<br>
+
+<table>
+   <tr>
+      <td>Panjang</td>
+      <td>=</td>
+      <td><?php echo "$Panjang "; ?></td>
+      <td>cm</td>
+   </tr>
+      <tr>
+      <td>Lebar</td>
+      <td>=</td>
+      <td><?php echo "$Lebar"; ?></td>
+      <td>cm</td>
+   </tr>
+      <tr>
+      <td>Luas</td>
+      <td>=</td>
+      <td><?php echo "$Luas"; ?></td>
+      <td>cm</td>
+   </tr>
+</table>
+</body>
+</html>
